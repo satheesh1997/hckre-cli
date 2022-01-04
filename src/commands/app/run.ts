22c50513f155c7fd1c5d047b2d2c6a7f54732e9c
Satheesh Kumar <mail@satheesh.dev>
2022-01-04 15:25:59 +0530

    fix: breaking of app:run when availableCommands in empty

--- src/commands/app/run.ts
+++ src/commands/app/run.ts
@@ -1,6 +1,11 @@
+import chalk from 'chalk'
+
+import {cli} from 'cli-ux'
+
 import {selectCommandToRun} from '../../prompts/app.prompts'
 import {Process} from '../../processes/process'
 import {AppCommonCommand} from '../../common/commands/app.command'
+import {printAddAppCommand} from '../../messages/info.messages'
 
 export class RunCommand extends AppCommonCommand {
   static description = 'Run a command'
@@ -8,17 +13,23 @@ export class RunCommand extends AppCommonCommand {
   async run(): Promise<void> {
     const availableCommands: {name: string; value: string}[] = []
 
-    for (const command in this.appConfiguration.commands) {
-      if (command) {
-        const question: {name: string; value: string} = {
-          name: command,
-          value: this.appConfiguration.commands[command] as unknown as string,
+    if (availableCommands.length === 0) {
+      cli.info(chalk.bold(chalk.redBright('\nNo command configured to run !!')))
+      cli.info(chalk.bold(chalk.cyanBright('\nUsefull Commands:')))
+      printAddAppCommand()
+    } else {
+      for (const command in this.appConfiguration.commands) {
+        if (command) {
+          const question: {name: string; value: string} = {
+            name: command,
+            value: this.appConfiguration.commands[command] as unknown as string,
+          }
+          availableCommands.push(question)
         }
-        availableCommands.push(question)
       }
-    }
 
-    const {command} = await selectCommandToRun(availableCommands)
-    Process.execSync(command)
+      const {command} = await selectCommandToRun(availableCommands)
+      Process.execSync(command)
+    }
   }
 }
